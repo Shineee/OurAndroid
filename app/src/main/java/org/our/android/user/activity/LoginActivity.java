926f4b396a9f986e3ecdc29db5123b77f8dd152c
Shine <wyhdgx@163.com>
2016-07-22 16:19:36 +0800

    0.0.8.20160722_base

--- app/src/main/java/org/our/android/user/activity/LoginActivity.java
+++ app/src/main/java/org/our/android/user/activity/LoginActivity.java
@@ -3,6 +3,7 @@ package org.our.android.user.activity;
 import android.os.Bundle;
 import android.support.design.widget.TextInputLayout;
 import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
 
 import org.our.android.R;
 
@@ -13,6 +14,8 @@ import butterknife.ButterKnife;
  * Created by wangyang on 2016/6/21.
  */
 public class LoginActivity extends AppCompatActivity {
+    @BindView(R.id.tbTitleMenu)
+    Toolbar tbTitleMenu;
     @BindView(R.id.tilUserName)
     TextInputLayout tilUserName;
     @BindView(R.id.tilPassword)
@@ -23,10 +26,10 @@ public class LoginActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.layout_login);
         ButterKnife.bind(this);
-        //Log.d("LoginActivity", "debug=" + BuildConfig.DEBUG + " version=" + BuildConfig.BUILD_TYPE);
         initView();
     }
 
     private void initView() {
+        setSupportActionBar(tbTitleMenu);
     }
 }
