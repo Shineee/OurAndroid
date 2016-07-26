692a0142b3529297e2da51283b608f70d2f2e8db
Shine <wyhdgx@163.com>
2016-07-26 17:21:32 +0800

    0.0.12.20160726_base

--- app/src/main/java/org/our/android/main/activity/MainActivity.java
+++ app/src/main/java/org/our/android/main/activity/MainActivity.java
@@ -3,8 +3,13 @@ package org.our.android.main.activity;
 import android.content.Intent;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
+import android.support.v4.widget.DrawerLayout;
+import android.support.v7.app.ActionBarDrawerToggle;
 import android.support.v7.app.AppCompatActivity;
-import android.widget.Button;
+import android.support.v7.widget.Toolbar;
+import android.view.View;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
 
 import org.our.android.R;
 import org.our.android.user.activity.LoginActivity;
@@ -18,16 +23,49 @@ import butterknife.OnClick;
  * Created by Administrator on 2016/7/24.
  */
 public class MainActivity extends AppCompatActivity {
+    @BindView(R.id.tbTopMenu)
+    Toolbar tbTopMenu;
+    @BindView(R.id.dlLeftMenu)
+    DrawerLayout dlLeftMenu;
+    private ActionBarDrawerToggle mActionBarDrawerToggle;
+    @BindView(R.id.lvLeftMenu)
+    ListView lvLeftMenu;
+    private String[] mLeftMenuArray = {"首页"};
+    private ArrayAdapter mArrayAdapter;
 
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.layout_main);
         ButterKnife.bind(this);
+        initView();
     }
 
-    @OnClick(R.id.btnLogin)
-    public void btnLogin() {
+    private void initView() {
+        setSupportActionBar(tbTopMenu);
+        getSupportActionBar().setHomeButtonEnabled(true); //设置返回键可用
+        getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+        //创建返回键，并实现打开关/闭监听
+        mActionBarDrawerToggle = new ActionBarDrawerToggle(this, dlLeftMenu, tbTopMenu, 0, 0) {
+            @Override
+            public void onDrawerOpened(View drawerView) {
+                super.onDrawerOpened(drawerView);
+            }
+
+            @Override
+            public void onDrawerClosed(View drawerView) {
+                super.onDrawerClosed(drawerView);
+            }
+        };
+        mActionBarDrawerToggle.syncState();
+        dlLeftMenu.addDrawerListener(mActionBarDrawerToggle);
+        //设置菜单列表
+        mArrayAdapter = new ArrayAdapter(this, android.R.layout.simple_list_item_1, mLeftMenuArray);
+        lvLeftMenu.setAdapter(mArrayAdapter);
+    }
+
+    @OnClick(R.id.imgAvatar)
+    public void imgAvatar() {
         Intent intent = new Intent(this, LoginActivity.class);
         startActivity(intent);
     }
